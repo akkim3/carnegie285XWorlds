03d9fd1ce184a9be73db0382d7667c6c899dc741
akkim3 <akkim3@gmail.com>
2019-03-27 19:37:35 -0500

    Added Base Auto Stuff imported from previous Robot

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -11,9 +11,39 @@
  * will be stopped. Re-enabling the robot will restart the task, not re-start it
  * from where it left off.
  */
+ auto driveA = ChassisControllerFactory::create(
+   {1,9},
+   {-10,-2},
+   AbstractMotor::gearset::green,
+   {4.125_in, 12.125_in}
+ );
+ AsyncMotionProfileController profileController = AsyncControllerFactory::motionProfile(
+   1,  // Maximum linear velocity of the Chassis in m/s
+   2.0,  // Maximum linear acceleration of the Chassis in m/s/s
+   2.5, // Maximum linear jerk of the Chassis in m/s/s/s
+  driveA // Chassis Controller
+ );
+QLength sideCapDistance = 3.5_ft;
+QLength frontFlagDistance = 4.5_ft;
+QLength frontCapDistance = 4.0_ft;
+QLength midFlagDistance = 1.0_ft;
+QLength platformAlignDistance = -4.0_ft;
+QLength alliancePlatformDistance = 4.0_ft;
+QLength centerPlatformDistance = 6.0_ft;
+auto pot = Potentiometer(1);
+auto scraper = AsyncControllerFactory::posPID(11,pot, 0.001, 0.0, 0.0001);
+ okapi::Motor intakeA {8};
+ okapi::Motor flywheelAuto {7};
+okapi::Motor indexerA {3};
 void autonomous() {
 //  profile.generatePath({Point{1_ft, 7ft, 0_deg}, Point{5_ft, 7_ft, 0_deg}}, "Cap"});
-
+ profileController.generatePath({
+  Point{0_ft, 0_ft, 0_deg},  //   (0, 0, 0)
+  Point{sideCapDistance, 0_ft, 0_deg}}, //
+  "Ball" // Profile name
+);
+profileController.setTarget("Ball");
+intakeA.moveVelocity(200);
 
 
 }
